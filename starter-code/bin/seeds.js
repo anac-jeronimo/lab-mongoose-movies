3e4a972496ca02c2fb7ebf635c127318f669b25c
anac-jeronimo <anac_jeronimo@hotmail.com>
2020-11-11 09:54:41 +0000

    seeds for movies and schema

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+/* const mongoose = require('mongoose');
 const Celebrity = require('../models/Celebrity.js');
 const DB_NAME = 'express-celebrity';
 
@@ -33,3 +33,40 @@ Celebrity.create(celebrity)
     mongoose.connection.close();
 })
 .catch(err => console.log(`An error occurred while creating books from the DB: ${err}`));
+*/
+
+const mongoose = require('mongoose');
+const Movies = require('../models/Movies.js');
+const DB_NAME = 'express-movies';
+
+mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
+    useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const movies = [
+    {
+        title: "V Wars",
+        genre: "Sci-fy",
+        plot: "Lorem ipsum dolor sit amet"
+    }, 
+    {
+        title: "Sweeney Todd",
+        genre: "Musical",
+        plot: "Lorem ipsum dolor sit amet"
+    },
+    {
+        title: "Black Swan",
+        genre: "Drama",
+        plot: "Lorem ipsum dolor sit amet"
+    }
+]
+
+
+Movies.create(movies)
+.then(moviesFromDB => {
+    console.log(`Created ${moviesFromDB.length} movies`);
+    mongoose.connection.close();
+})
+.catch(err => console.log(`An error occurred while creating movies from the DB: ${err}`));
